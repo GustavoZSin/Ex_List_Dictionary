ef9440221f39967326c8a6ab507b5c18cd814912
gustavo <gustavo.singeski@gmail.com>
2024-05-20 21:45:49 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_List_Dictionary/CadProdutos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_List_Dictionary
+{
+    public class CadProdutos
+    {
+        private List<Produto> produtos;
+        public CadProdutos()
+        {
+            produtos = new List<Produto>();
+        }
+        public void Adicionar(Produto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        public void Listar()
+        {
+            foreach(Produto produto in produtos)
+            {
+                Console.WriteLine(produto);
+            }
+        }
+
+        public string Pesquisar(string nomeProduto)
+        {
+            Produto produto = produtos.Find(p => p.Nome == nomeProduto);
+
+            return produto != null ? produto.ToString() : "Produto não encontrado!";
+        }
+
+        public void AtualizarPreco(string nomeProduto, double novoValor)
+        {
+            produtos.ForEach(p =>
+            {
+                if (p.Nome.Equals(nomeProduto))
+                {
+                    p.Preco = novoValor;
+                }
+            });
+        }
+
+        public void Remover(string nomeProduto)
+        {
+            produtos.Remove(produtos.Find(p => p.Nome.Equals(nomeProduto)));
+        }
+    }
+}
---
+++ Ex_List_Dictionary/Nota.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_List_Dictionary
+{
+    public class Nota
+    {
+        private int id;
+        private string materia;
+        private int valor;
+        public Nota(int id, string materia, int valor)
+        {
+            this.id = id;
+            this.materia = materia;
+            this.valor = valor;
+        }
+        public Nota()
+        {
+            id = 0;
+            materia = string.Empty;
+            valor = 0;
+        }
+        public int Id { get { return id; } set { id = value; } }
+        public string Materia { get { return materia; } set { materia = value; } }
+        public int Valor { get { return valor; } set { valor = value; } }
+    }
+}
